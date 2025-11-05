007a5d50af0c33a23608d4835c4eb0a0818061e8
kamranawan1234 <145137513+kamranawan1234@users.noreply.github.com>
2025-11-05 15:12:03 -0500

    fix

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") or "eref jnyh bfcz zwpu"
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "True") == "True"
     MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL", "False") == "True"
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER") or "awank6107@gmail.com"
+    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER") or "Fitness Application"
 
     # Base URL used to build verification links (adjust for production)
     APP_BASE_URL = os.environ.get("APP_BASE_URL") or "http://127.0.0.1:5000"
